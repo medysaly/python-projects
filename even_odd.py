51c89999e70721d1711414d743cbbbcc17781d74
mehdisalhi <medysalywork@gmail.com>
2025-04-24 14:22:59 -0400

    even_odd.py for Day 2 exercise

--- even_odd.py
+++ even_odd.py
@@ -1,6 +1,19 @@
-number = int(input("Please enter a number: "))
+score = int(input('Enter your score: '))
 
-if number % 2 == 0:
-    print(f" {number} is even.")
+if 90 <= score <= 100:
+    print('You got an A!')
+
+elif 80 <= score <= 89:
+    print('You got a B!')
+
+elif 70 <= score <= 79:
+    print('You got a C!')
+
+elif 60 <= score <= 69:
+    print('You got a D!')
+
+elif score < 60:
+    print('You got a F!')
+
 else:
-    print(f"{number} is odd.")
+    print('Please enter the correct number.')
