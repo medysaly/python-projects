76645417e829d1ca02e4018821e6b4a611eacd01
mehdisalhi <medysalywork@gmail.com>
2025-04-25 15:08:15 -0400

    temp_converter

--- day3_ex3_discount_check.py
+++ day3_ex3_discount_check.py
@@ -1,14 +1,16 @@
-# Ask the user for their age
-# Ask if the user is a student (yes or no)
-# If they are younger than 18 OR they are a student,
-# print "You get a discount!"
-# Otherwise, print "No discount, sorry."
-
 age = int(input('Enter your age: '))
-student = input('Are you a student? ').lower().strip()
 
-if age < 18 or student == 'yes':
-    print('You get a discount! ')
+if age < 12:
+    adult = input('Are you with an adult? ').strip().lower()
+    if adult == 'yes':
+        print('Child ticket with adult: $5')
+    else:
+        print('Sorry, you must be with an adult.')
 
-else:
-    print('No discount, sorry.')
+elif age > 12:
+    student = input('Are you a student? ').lower().strip()
+    if student == 'yes':
+        print('Student ticket: $8')
+
+    else:
+        print('Regular ticket: $12')
