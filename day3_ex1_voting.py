32f44277f1a5bcbe4d6e296cd19c2fd72651db77
mehdisalhi <medysalywork@gmail.com>
2025-04-25 14:41:52 -0400

    temp_converter

---
+++ day3_ex1_voting.py
@@ -0,0 +1,10 @@
+# Ask the user for their age
+# If age is 18 or more, print "You can vote!"
+# Otherwise, print "You are too young to vote."
+
+age = int(input('Enter your age : '))
+
+if age >= 18:
+    print("You can vote!")
+else:
+    print('You are too young to vote')
