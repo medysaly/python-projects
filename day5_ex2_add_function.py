aac046b9e2b9b8a6a3118cbe6c18d9e9d66778c2
mehdisalhi <medysalywork@gmail.com>
2025-04-28 13:34:58 -0400

    temp_converter

---
+++ day5_ex2_add_function.py
@@ -0,0 +1,8 @@
+def add_numbers():
+    num1 = int(input('Enter number 1: '))
+    num2 = int(input('Enter number 2 : '))
+    sum = num1 + num2
+    return sum
+
+result = add_numbers()
+print(f'The sum is : {result}')
