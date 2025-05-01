05a57ce1daebcf2d19c8333ace149ea62e161552
mehdisalhi <medysalywork@gmail.com>
2025-05-01 14:54:23 -0400

    temp_converter

---
+++ day6_ex1_fav_foods.py
@@ -0,0 +1,9 @@
+
+food = ['Pizza', 'Steak', 'Pasta']
+food.append('Tacos')
+food.append('Burritos')
+food.sort()
+total_items = len(food)
+print(f'We have {total_items} in the list:')
+for i in food:
+    print(i)
