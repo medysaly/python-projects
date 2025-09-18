a05abe7686d5a75a63a1cb51d0a276351d36cea6
Mehdi Salhi <medysalywork@gmail.com>
2025-09-18 15:17:19 -0400

    add day1_practice_cloud.py: simple user info print

---
+++ day1_practice_cloud.py
@@ -0,0 +1,6 @@
+name = "Mehdi"
+age = 35
+is_student = True
+favorite_cloud = "AWS"
+
+print(f"My name is {name}, I am {age} years old, and my favorite cloud platform is {favorite_cloud}. Am I a student? {is_student}")
