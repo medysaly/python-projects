40a2ccfb8c571094cbaf59bfafba5a9acaba9a80
Mehdi Salhi <medysalywork@gmail.com>
2025-09-18 15:50:35 -0400

    add day1_practice3_cloud.py: sample cloud items and types

---
+++ day1_practice3_cloud.py
@@ -0,0 +1,6 @@
+item_1 = "EC2 Instance"
+item_2 = "S3 Bucket"
+item_3 = "Lambda Function" 
+
+print(f"First item: {item_1} , Second item: {item_2} , Third item: {item_3}")
+print(type(item_1), type(item_2), type(item_3))
