53436d01b5f86f33d3ce115970743ce643aac40e
Mehdi Salhi <medysalywork@gmail.com>
2025-10-17 19:08:45 -0400

    day12_practice3_cloud.py

--- day11_prectice2_cloud.py
+++ day11_prectice2_cloud.py
@@ -2,6 +2,7 @@ try:
     file_name = input("Enter the file name: ")
     if not file_name:
         raise ValueError("File name cannot be empty.")
+    
 
 except ValueError:
     print("Invalid input. Please enter a valid file name.")
