5d49cf629e7f488b65539294c9f07830068f51f3
Mehdi Salhi <medysalywork@gmail.com>
2025-12-31 14:39:10 -0500

    cloud_practice9.py

--- cloud_practice9.py
+++ cloud_practice9.py
@@ -2,8 +2,9 @@ import boto3
 
 s3 = boto3.client('s3')
 bucket_name = "mehdi-cloud-2"
-
+s3.delete_object(Bucket=bucket_name, Key='one.png')
 response = s3.list_objects_v2(Bucket=bucket_name)
+
 
 print(f'Objects in S3 bucket: {bucket_name}')
 print('-----------------------------------')   
